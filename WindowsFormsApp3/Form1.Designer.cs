fc97f1c1e10c94ff56556897f59fb1f9d54fff94
And <fifafifa12345@inbox.ru>
2022-05-18 18:42:31 +0300

    start9

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 namespace WindowsFormsApp3
 {
     partial class Form1
